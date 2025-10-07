a61605293fdb98b3c2a370ad797f132f82b02a68
Asagar Ali <asagar.code@gmail.com>
2025-10-07 17:42:48 +1100

    Update CORS for production

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,21 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: [
+      'FRONTEND_URL',
+      'orders-management-frontend-jet.vercel.app',
+      /\.vercel\.app$/,
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 8000);
 }
